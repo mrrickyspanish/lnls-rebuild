62b43878d20334a9d44df13b7dae7f778e188613
RJ Barnes <rj@creativeeyemultimedia.com>
2025-10-29 23:48:14 -0500

    Fix Sanity schemas import path

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,60 +3,15 @@ import { structureTool } from 'sanity/structure'
 import { visionTool } from '@sanity/vision'
 import { media } from 'sanity-plugin-media'
 
-import { schemaTypes } from './schemas'
+import { schemaTypes } from './studio/schemas'
 
 export default defineConfig({
   name: 'lnls-studio',
-  title: 'LNLS Content Studio',
-  
+  title: 'LNLS Platform',
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
-  
   basePath: '/studio',
-  
-  plugins: [
-    structureTool({
-      structure: (S) =>
-        S.list()
-          .title('Content')
-          .items([
-            S.listItem()
-              .title('Articles')
-              .child(
-                S.documentTypeList('article')
-                  .title('Articles')
-                  .filter('_type == "article"')
-              ),
-            S.divider(),
-            S.listItem()
-              .title('Podcast Episodes')
-              .child(
-                S.documentTypeList('episode')
-                  .title('Episodes')
-                  .filter('_type == "episode"')
-              ),
-            S.divider(),
-            S.listItem()
-              .title('Authors')
-              .child(
-                S.documentTypeList('author')
-                  .title('Authors')
-                  .filter('_type == "author"')
-              ),
-            S.divider(),
-            S.listItem()
-              .title('Settings')
-              .child(
-                S.document()
-                  .schemaType('siteSettings')
-                  .documentId('siteSettings')
-              ),
-          ]),
-    }),
-    visionTool(),
-    media(),
-  ],
-  
+  plugins: [structureTool(), visionTool(), media()],
   schema: {
     types: schemaTypes,
   },
